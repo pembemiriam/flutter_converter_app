7df3752f844a6db60206f9ebd6ae00d63046c9e6
pembemiriam <pembemiriam007@gmail.com>
2019-11-28 14:15:39 +0100

    update to actual conversion

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -2,11 +2,24 @@ import 'package:converter_app/category.dart';
 import 'package:converter_app/unit.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
 
   final String title;
 
-  MyHomePage({this.title, Key key}): super(key: key);
+  MyHomePage({this.title, Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyHomePageState();
+  }
+
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  MyHomePage page = new MyHomePage();
+  final categories = <Category>[];
+
 
   static const _categoriesNames = <String>[
     'Length',
@@ -41,34 +54,36 @@ class MyHomePage extends StatelessWidget{
     });
   }
 
-
   @override
-  Widget build(BuildContext context) {
-    final categories = <Category>[];
-
+  void initState() {
+    // TODO: implement initState
+    super.initState();
     for (var i = 0; i < _categoriesNames.length; i++ ){
       categories.add(Category(name: _categoriesNames[i], color: _categoryColors[i], iconLocation: Icons.cake,units: _retrieveUnitList(_categoriesNames[i]),
       ));
 
     }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
     // TODO: implement build
     return
       Scaffold(
         appBar: AppBar(
           title: Center(
-            child: Text(title,
+            child: Text('Unit Converter',
             ),
           ),
         ),
        body: Container(
+         color: Colors.pink.shade50,
       child: _categories(categories)
     ));
   }
-
-
-
-
-
 
 
 }
--- lib/category-route.dart
+++ lib/category-route.dart
@@ -6,6 +6,7 @@ import 'package:converter_app/unit.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
+const _padding = EdgeInsets.all(16.0);
 
 /// Converter screen where users can input amounts to convert.
 ///
@@ -13,7 +14,7 @@ import 'package:meta/meta.dart';
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   /// Color for this [Category].
   final Color color;
 
@@ -28,30 +29,198 @@ class ConverterRoute extends StatelessWidget {
         assert(units != null);
 
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ConverterRouteState();
+  }
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  List<DropdownMenuItem> _unitMenuItems;
+  bool _showValidationError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _createDropDownMenuItems();
+    _setDefaults();
+  }
+
+  void _createDropDownMenuItems() {
+    var newItems = <DropdownMenuItem>[];
+    for (var unit in widget.units) {
+      newItems.add(DropdownMenuItem(
+          value: unit.name,
+          child: Container(
+            child: Text(
+              unit.name,
+              softWrap: true,
+            ),
+          )));
+    }
+
+    setState(() {
+      _unitMenuItems = newItems;
+    });
+  }
+
+  void _setDefaults() {
+    setState(() {
+      _fromValue = widget.units[0];
+      _toValue = widget.units[1];
+    });
+  }
+
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  void _updateConversion() {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateInputValue(String input) {
+    setState(() {
+      if (input == null || input.isEmpty) {
+        _convertedValue = '';
+      } else {
+        try {
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateConversion();
+        } on Exception catch (e) {
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
+  Unit _getUnit(String unitName) {
+    return widget.units.firstWhere((Unit unit) {
+      return unit.name == unitName;
+    }, orElse: null);
+  }
+
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+      _fromValue = _getUnit(unitName);
+    });
+
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      _toValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  Widget _createDropDown(String currentValue, ValueChanged<dynamic> onChanged) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+          color: Colors.grey[50],
+          border: Border.all(color: Colors.grey[400], width: 1.0)),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[50],
+        ),
+        child: DropdownButtonHideUnderline(
+            child: ButtonTheme(
+                alignedDropdown: true,
+                child: DropdownButton(
+                    value: currentValue,
+                    items: _unitMenuItems,
+                    onChanged: onChanged,
+                    style: Theme.of(context).textTheme.title))),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
-    final unitWidgets = units.map((Unit unit) {
-      return Container(
-        color: color,
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
-            ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }).toList();
+    final input = Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextField(
+              style: Theme.of(context).textTheme.display1,
+              decoration: InputDecoration(
+                  labelStyle: Theme.of(context).textTheme.display1,
+                  errorText:
+                      _showValidationError ? 'Invalid number entered' : null,
+                  labelText: 'Input',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(0.0),
+                  )),
+              keyboardType: TextInputType.number,
+              onChanged: _updateInputValue),
+          _createDropDown(_fromValue.name, _updateFromConversion)
+        ],
+      ),
+    );
 
-    return ListView(
-      children: unitWidgets,
+    final arrows = RotatedBox(
+        quarterTurns: 1, child: Icon(Icons.compare_arrows, size: 40.0));
+
+    final output = Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          InputDecorator(
+            child: Text(
+              _convertedValue,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            decoration: InputDecoration(
+                labelText: 'Output',
+                labelStyle: Theme.of(context).textTheme.display1,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(0.0),
+                )),
+          ),
+          _createDropDown(_toValue.name, _updateToConversion)
+        ],
+      ),
+    );
+
+    final converter = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[input, arrows, output],
+    );
+
+    return Padding(
+      padding: _padding,
+      child: converter,
     );
   }
 }
